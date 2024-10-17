0ccb5d19b0163894765ee2a5e3eab1333e595190
neeraj <neeraj.kumar@antiersolutions.com>
2024-10-17 16:52:25 +0530

    added ENV file and made configuration changes with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,21 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
+    ConfigModule.forRoot({isGlobal:true}),
     TypeOrmModule.forRoot({  // Database Connection
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.MYSQL_HOST,
       port: 3306,
-      username: 'root',
-      password: 'abcd',
-      database: 'polling_system',
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
       entities: [User],
-      synchronize: true,
+      synchronize: true, 
     }),
   ],
   controllers: [],
